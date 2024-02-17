20773a82ae0626a636eb25adba8bab19864d7983
medanis7 <laouaranis0@gmail.com>
2024-02-17 19:00:32 +0100

    this new update contains

--- lib/shared/snackbar.dart
+++ lib/shared/snackbar.dart
@@ -4,6 +4,6 @@ showSnackBar(BuildContext context, String text) {
     return ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       duration: Duration(seconds: 5),
       content: Text(text),
-      action: SnackBarAction(label: "close", onPressed: () {}),
+      action: SnackBarAction(label: "Close", onPressed: () {}),
     ));
  }
